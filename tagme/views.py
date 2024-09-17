3bd0c7ab25564a1e22e1f8686190597e1d9143eb
jessicadpo <158501505+jessicadpo@users.noreply.github.com>
2024-09-17 16:50:50 -0400

    Create views.py

---
+++ tagme/views.py
@@ -0,0 +1,47 @@
+"""Module for litereview views"""
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm, LoginForm
+
+
+def homepage(request):
+    """View for index page (AKA homepage)"""
+    #if request.method == 'POST':
+        #advanced_search()
+    return render(request, 'homepage.html')
+
+
+def signup_login(request):
+    """View for Sign up/Login page"""
+    # NOTE: USERNAMES MUST BE UNIQUE
+    if request.method == 'POST':
+        if 'signup-submit' in request.POST:
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password1')
+                user = authenticate(request, username=username, password=password)
+                login(request, user)
+                return redirect("user-profile-page", username=username)
+            forms = {"signup_form": form, "login_form": LoginForm()}
+            return render(request, 'signup-login.html', {'forms': forms})
+
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect("user-profile-page", username=username)
+        forms = {"signup_form": SignUpForm(), "login_form": form}
+        return render(request, 'signup-login.html', {'forms': forms})
+    forms = {"signup_form": SignUpForm(), "login_form": LoginForm()}
+    return render(request, 'signup-login.html', {'forms': forms})
+
+
+def logout_view(request):
+    """View for Logout Functionality"""
+    logout(request)
+    return redirect("homepage")
