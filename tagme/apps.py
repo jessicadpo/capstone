3fc39b44d899eaf25db576932362a7b2f90375ce
jessicadpo <158501505+jessicadpo@users.noreply.github.com>
2024-09-17 16:42:50 -0400

    Create apps.py

---
+++ tagme/apps.py
@@ -0,0 +1,8 @@
+"""Configuration file for Django app"""
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Class for implementing app configuration"""
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tagme'
