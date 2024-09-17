258adc15ee83d199bf304f6b2f2c84d5b84e7b2d
jessicadpo <158501505+jessicadpo@users.noreply.github.com>
2024-09-17 16:42:24 -0400

    Create admin.py

---
+++ tagme/admin.py
@@ -0,0 +1,7 @@
+"""Module for registering models (i.e., SQLite3 database tables)"""
+
+from django.contrib import admin
+#from .models import ModelClassName
+
+# Register your models here.
+#admin.site.register(ModelClassName)
