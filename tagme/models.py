6a8ba716d3c651f3d9a1921a9025311b4138d475
Jessica Delorme-Po <jessicadelormepo@cmail.carleton.ca>
2024-11-08 19:21:51 -0500

    Python fixes to pass linter

--- tagme/models.py
+++ tagme/models.py
@@ -2,3 +2,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UserContributions(models.Model):
+    """Class for UserContributions database table"""
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    # TODO: Add rest of attributes (just doing this to pass linter for now)
--- tagme/views.py
+++ tagme/views.py
@@ -1,6 +1,5 @@
 """Module for TagMe views"""
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from .forms import SignUpForm, LoginForm
 
