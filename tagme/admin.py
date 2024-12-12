8e963c009761473404d71f78600ff1fab63c42b6
Micah VanEwyk <micah@Micahs-MacBook-Pro-3.local>
2024-12-11 19:04:53 -0500

    Updated Admin Report model to display report information + filters)

--- tagme/admin.py
+++ tagme/admin.py
@@ -9,4 +9,18 @@ admin.site.register(Tag)
 admin.site.register(Reward)
 admin.site.register(UserProfile)
 admin.site.register(UserContribution)
-admin.site.register(Report)
+
+class ReportAdmin(admin.ModelAdmin):
+    '''
+    Admin view for reporting.
+    Creates display fields, filters, and then search fields for navigation.
+    '''
+    list_display = ('report_id', 'item_id', 'user_id', 'tag', 'reason', 'creation_datetime', 'decision')
+    list_filter = ('tag', 'user_id', 'decision', 'creation_datetime')
+    search_fields = ('reason', 'user_id__username', 'tag__tag')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs
+
+admin.site.register(Report, ReportAdmin)
