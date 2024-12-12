87de2f23c090ed28c3b8754e26266ea31298df42
Micah VanEwyk <micah@Micahs-MacBook-Pro-3.local>
2024-12-11 19:06:59 -0500

    Edited fields in order of assumed priority, happy to change upon request.

--- tagme/admin.py
+++ tagme/admin.py
@@ -16,7 +16,7 @@ class ReportAdmin(admin.ModelAdmin):
     Creates display fields, filters, and then search fields for navigation.
     '''
     list_display = ('report_id', 'item_id', 'user_id', 'tag', 'reason', 'creation_datetime', 'decision')
-    list_filter = ('tag', 'user_id', 'decision', 'creation_datetime')
+    list_filter = ('tag', 'creation_datetime', 'decision', 'user_id')
     search_fields = ('reason', 'user_id__username', 'tag__tag')
 
     def get_queryset(self, request):
