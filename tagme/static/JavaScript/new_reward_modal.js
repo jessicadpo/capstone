f2d6e8d5ea7726839ed551f6d45dc367147685e2
Jessica Delorme-Po <jessicadelormepo@cmail.carleton.ca>
2024-12-22 12:10:46 -0500

    New Reward redesigned

--- tagme/static/JavaScript/new_reward_modal.js
+++ tagme/static/JavaScript/new_reward_modal.js
@@ -2,7 +2,8 @@ const newRewardOverlay = document.getElementById('new-reward-overlay');
 const newRewardModal = document.getElementById('new-reward-modal');
 const newReward = document.querySelectorAll('#new-reward-modal .reward-title span');
 const skipEquipButton = document.getElementById('skip-equip-button');
-const yesEquipButton = document.getElementById('yes-equip-button');
+const slot1EquipButton = document.getElementById('slot-1-equip-button');
+const slot2EquipButton = document.getElementById('slot-2-equip-button');
 const fireworks = document.querySelectorAll('.firework');
 
 const equipForm = document.getElementById('equip-title-form');
@@ -55,10 +56,10 @@ function playFireworks() {
     }, 7000);
 }
 
-function equipTitle() {
+function equipTitle(slotNumber) {
     // Fill hidden form fields with new title to equip
     titleToEquipFormField.value = newReward[0].textContent;
-    equipSlotFormField.value = "1"; // Always equip new rewards as Title #1 TODO: Change?
+    equipSlotFormField.value = slotNumber;
     equipForm.submit();
 }
 
@@ -73,10 +74,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // Set close modal button behaviour
     skipEquipButton.addEventListener('click', closeNewRewardModal);
 
-    // Set "Yes" (Equip) button behaviour
-    yesEquipButton.addEventListener('click', function() {
+    // Set "Equip in Slot #1" button behaviour
+    slot1EquipButton.addEventListener('click', function() {
         event.preventDefault();
-        equipTitle();
+        equipTitle("1");
+        closeNewRewardModal();
+    });
+
+    // Set "Equip in Slot #2" button behaviour
+    slot2EquipButton.addEventListener('click', function() {
+        event.preventDefault();
+        equipTitle("2");
         closeNewRewardModal();
     });
 
